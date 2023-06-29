e80afd1002d18935f5d43d8a5277c68ce1bda7a0
Allen <allendev@alphacode.co.kr>
2023-06-29 14:22:33 +0900

    6장 어셈블리 완료

--- projects/06/assembler/src/Main.java
+++ projects/06/assembler/src/Main.java
@@ -1,14 +1,17 @@
+import javax.security.auth.callback.Callback;
 import java.io.*;
+import java.util.Scanner;
 
 public class Main {
 
 	public static void main(String[] args) {
 
-		if (args.length == 0) {
-			System.out.println("Please input File Name!");
-		}
+		Scanner sc = new Scanner(System.in);
+		System.out.print("Please enter the path of the file to run: ");
+		String filePath = sc.nextLine();
+		sc.close();
 
-		for (String filePath : args) {
+			System.out.println("convert to binary process START: " + filePath);
 			try {
 					if (!filePath.endsWith(".asm")) {
 						throw new FileNotFoundException();
@@ -16,78 +19,97 @@ public class Main {
 
 					File file = new File(filePath);
 					SymbolTable symbolTable = createSymbolTable(file);
+
+					// parser, code 모듈 초기화
+					Parser parser = new Parser(new BufferedReader(new FileReader(file)));
 					Code code = new Code();
 
-					String fileName = file.getName().split("\\.")[0];
-					File hackFile = new File(file.getAbsolutePath().replace(file.getName(), "") + fileName + ".hack");
-
+					// 변환한 기계어 저장할 파일 및 스트림 생성
+					String hackFilePath = file.getCanonicalPath().replace(".asm", ".hack");
+					File hackFile = new File(hackFilePath);
 					BufferedWriter writer = new BufferedWriter(new FileWriter(hackFile));
-					Parser parser = new Parser(new BufferedReader(new FileReader(file)));
 
+					// SymbolTable에 할당되지 않은 변수는 16번 메모리부터 할당 (15번까지는 예약됨)
 					int variableAddress = 16;
 
+					// 파일의 끝까지 읽음
 					while(parser.hasMoreLines()) {
+						// 다음 라인으로 이동
 						parser.advance();
 						// 주석이거나 빈 라인인 경우 스킵
 						if (parser.currentLine.equals("")) continue;
 
-						// C-명령어인 경우 comp, dest, jump 파싱하여 write
+						// C-명령어인 경우
 						if (parser.instructionType() == InstructionType.C_INSTRUCTION) {
+							// 어셈블리어를 comp, dest, jump로 파싱하여 저장
 							writer.write("111" + code.comp(parser.comp()) + code.dest(parser.dest()) + code.jump(parser.jump()) + "\n");
 						}
 
-						// A-명령어인 경우 2진수로 변환하여 write
+						// A-명령어인 경우
 						if (parser.instructionType() == InstructionType.A_INSTRUCTION) {
 							String symbol = parser.symbol();
-
 							String address;
+
 							// symbol이 숫자일 경우
-							if (symbol.chars().allMatch(Character::isDigit)) {
+							if (symbolIsNumber(symbol)) {
+								// 2진수로 변환
 								address = Integer.toBinaryString(Integer.parseInt(symbol));
 							} else if (symbolTable.contains(symbol)) { // 문자일 경우 && symbolTable에 존재할 경우
-								address = Integer.toBinaryString(symbolTable.getAddress(symbol) + 16);
+								// symbolTable에서 주소를 가져와 2진수로 변환
+								address = Integer.toBinaryString(symbolTable.getAddress(symbol));
 							} else {
+								// symbolTable에 존재하지 않을 경우 symbolTable에 추가하고 2진수로 변환
 								symbolTable.addEntry(symbol, variableAddress);
-								variableAddress++;
 								address = Integer.toBinaryString(variableAddress);
+								variableAddress++;
 							}
+							// 변환한 2진수를 16비트로 변환 후 저장
 							String address16Bit = String.format("%016d", Long.parseLong(address));
 							writer.write(address16Bit + "\n");
 						}
-
 					}
 
 					writer.flush();
 					writer.close();
+					System.out.println("convert to binary process SUCCESS: " + hackFile.getName());
 			} catch (FileNotFoundException e) {
-				if (!filePath.endsWith(".asm")) {
-					System.out.println("File extension is not .asm");
-				} else {
+				if (filePath.endsWith(".asm")) {
 					e.printStackTrace();
 				}
+				System.out.println("File extension is not .asm");
 			} catch (IOException e) {
 				throw new RuntimeException(e);
+			} finally {
+				System.out.println("convert to binary process END: " + filePath);
 			}
-
-
-		}
-
 	}
 
-	static SymbolTable createSymbolTable(File file) throws FileNotFoundException {
+	static SymbolTable createSymbolTable(File file) throws IOException {
+		Reader f = new FileReader(file);
 		Parser parser = new Parser(new BufferedReader(new FileReader(file)));
-
 		SymbolTable symbolTable = new SymbolTable();
 		int lineNumber = 0;
 
 		while(parser.hasMoreLines()) {
 			parser.advance();
+			if (parser.currentLine.equals("")) continue;
+
 			switch(parser.instructionType()) {
 				case C_INSTRUCTION, A_INSTRUCTION -> lineNumber++;
-				case L_INSTRUCTION -> symbolTable.addEntry(parser.symbol(), lineNumber+1);
+				case L_INSTRUCTION -> {
+					if (symbolTable.contains(parser.symbol())) {
+						lineNumber++;
+					} else {
+						symbolTable.addEntry(parser.symbol(), lineNumber);
+					}
+				}
 			}
 		}
 
 		return symbolTable;
+	}
+
+	static boolean symbolIsNumber(String symbol) {
+		return symbol.chars().allMatch(Character::isDigit);
 	}
 }
--- projects/06/assembler/src/Parser.java
+++ projects/06/assembler/src/Parser.java
@@ -6,36 +6,9 @@ public class Parser {
 	private BufferedReader br;
 	private String nextLine;
 	public String currentLine;
-	private Map<String, Integer> reservedWord;
-
 
 	public Parser(BufferedReader br) {
 		this.br = br;
-		reservedWord = Map.ofEntries(
-				Map.entry("R0", 0),
-				Map.entry("R1", 1),
-				Map.entry("R2", 2),
-				Map.entry("R3", 3),
-				Map.entry("R4", 4),
-				Map.entry("R5", 5),
-				Map.entry("R6", 6),
-				Map.entry("R7", 7),
-				Map.entry("R8", 8),
-				Map.entry("R9", 9),
-				Map.entry("R10", 10),
-				Map.entry("R11", 11),
-				Map.entry("R12", 12),
-				Map.entry("R13", 13),
-				Map.entry("R14", 14),
-				Map.entry("R15", 15),
-				Map.entry("SP", 0),
-				Map.entry("LCL", 1),
-				Map.entry("ARG", 2),
-				Map.entry("THIS", 3),
-				Map.entry("THAT", 4),
-				Map.entry("SCREEN", 16384),
-				Map.entry("KBD", 24576)
-		);
 	}
 
 	boolean hasMoreLines() {
@@ -69,8 +42,7 @@ public class Parser {
 			throw new Error("invalid call function - symbol() only called when instruction type is 'L' or 'A'");
 		}
 
-		String symbol = currentLine.replaceAll("[(|)@]","");
-		return reservedWord.containsKey(symbol) ? reservedWord.get(symbol).toString() : symbol;
+		return currentLine.replaceAll("[(|)@]","");
 	}
 
 	String dest() {
