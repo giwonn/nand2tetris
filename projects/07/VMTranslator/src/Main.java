fd23811e143fc794b350d4486f4c89fd37fde944
Allen <allendev@alphacode.co.kr>
2023-07-06 15:03:32 +0900

    7장 리팩토링

--- projects/07/VMTranslator/src/Main.java
+++ projects/07/VMTranslator/src/Main.java
@@ -3,16 +3,16 @@ import java.io.IOException;
 public class Main {
 	public static void main(String[] args) throws IOException {
 		if (args.length > 1) {
-			throw new IllegalArgumentException("argument(filePath) is only one");
+			throw new IllegalArgumentException("Only one file path can be entered");
+		} else if (args.length == 0) {
+			throw new IllegalArgumentException("Please enter the file path");
 		}
 
-		String filePath = args[0];
-		Parser parser = new Parser(filePath);
-		CodeWriter codeWriter = new CodeWriter(filePath);
+		Parser parser = new Parser(args[0]);
+		CodeWriter codeWriter = new CodeWriter(args[0]);
 
 		while (parser.hasMoreLines()) {
 			parser.advance();
-
 			switch (parser.commandType()) {
 				case C_ARITHMETIC -> codeWriter.writeArithmetic(parser.arg1());
 				case C_PUSH, C_POP -> codeWriter.writePushPop(parser.commandType(), parser.arg1(), parser.arg2());
